2e01871427257cc077f371e4e0cdad1ec558ccde
sasra111 <sasithbandara12@gmail.com>
2024-09-15 18:46:54 +0530

    Initialize window

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+#create a window / initialize
+root = tk.Tk()
+root.title("Hello World")
+root.iconbitmap('Tick_Mark.ico')
+root.geometry('400x400')
+
+#run the window
+root.mainloop()
